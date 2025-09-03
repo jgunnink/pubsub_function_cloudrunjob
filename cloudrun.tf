8d979f93355f36164ba62058d971581a2d999808
JK Gunnink <jgunnink@gmail.com>
2025-09-03 13:58:16 +0800

    update job to echo out the message

--- cloudrun.tf
+++ cloudrun.tf
@@ -6,7 +6,8 @@ resource "google_cloud_run_v2_job" "default" {
   template {
     template {
       containers {
-        image = "us-docker.pkg.dev/cloudrun/container/job:latest"
+        image   = "debian:stable-slim"
+        command = ["/bin/echo"]
       }
     }
   }
